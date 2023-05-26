6c623d8acd3122bf48393ce27065e9815f8e16af
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2023-05-27 01:42:03 +0300

    retrofit api created

---
+++ app/src/main/java/com/sisifos/foodapp/Constants.kt
@@ -0,0 +1,12 @@
+package com.sisifos.foodapp
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "5460183f3ca2442086850908165652c6"
+
+    }
+
+}
---
+++ app/src/main/java/com/sisifos/foodapp/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.sisifos.foodapp
+
+class RemoteDataSource() {
+}

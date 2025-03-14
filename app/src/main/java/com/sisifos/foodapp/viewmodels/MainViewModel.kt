a3bb6ade321919270f149c71d147332e4ba49a84
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2025-03-14 15:35:28 +0300

    new notes

--- app/src/main/java/com/sisifos/foodapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/sisifos/foodapp/viewmodels/MainViewModel.kt
@@ -16,8 +16,6 @@ import retrofit2.Response
 import java.lang.Exception
 import javax.inject.Inject
 
-
-
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: Repository,
@@ -25,7 +23,7 @@ class MainViewModel @Inject constructor(
 ): AndroidViewModel(application) {
 
     /**
-     *ROOM
+     * ROOM
      */
     val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readRecipes().asLiveData()
 
@@ -34,25 +32,16 @@ class MainViewModel @Inject constructor(
             repository.local.insertRecipes(recipesEntity)
         }
 
-
-
     /**
-     *RETROFIT
+     * RETROFIT
      */
-
-    //inject kullandığımız için viewmodel factory kullanmamıza gerek yok arka planda kendisi bazı kodları oluşturuyor.
-    //kısaca yapılan işlem: istek atılır, bunların kontolü yapılır ve recipesResponse mutablelivedata sına kaydedilir.
-
     var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
-
-
 
     fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
         getRecipesSafeCall(queries)
     }
 
     private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
-
         recipesResponse.value = NetworkResult.Loading()
 
         if (hasInternetConnection()) {
@@ -64,7 +53,6 @@ class MainViewModel @Inject constructor(
                 if(foodRecipe != null) {
                     offlineCacheRecipes(foodRecipe)
                 }
-
             } catch (e: Exception) {
                 recipesResponse.value = NetworkResult.Error("Recipes not found.")
             }
@@ -78,21 +66,15 @@ class MainViewModel @Inject constructor(
         insertRecipes(recipesEntity)
     }
 
-    /*
-        Bu kod parçacığı, offlineCacheRecipes işlevini kullanarak FoodRecipe nesnesini RecipesEntity nesnesine dönüştürür ve
-        ardından bu dönüştürülmüş veriyi insertRecipes işlevine geçerek veritabanına ekler.
-        Bu, yemek tarifi verilerini çevrimdışı olarak önbelleğe almak için kullanılan bir adımdır.
-     */
-
     private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe> {
         when {
             response.message().toString().contains("timeout") -> {
-                return NetworkResult.Error("Timeout") //cevap gecikirse gelen hata timeout hatası olur
+                return NetworkResult.Error("Timeout")
             }
             response.code() == 402 -> {
                 return NetworkResult.Error("API Key Limited.")
             }
-            response.body()!!.results.isEmpty() -> { //cevap başarılı da olsa içi boş gelebilir api den.
+            response.body()!!.results.isEmpty() -> {
                 return NetworkResult.Error("Recipes not found.")
             }
             response.isSuccessful -> {
@@ -105,8 +87,6 @@ class MainViewModel @Inject constructor(
         }
     }
 
-
-    //bu fonksiyon mevcut ağ bağlantısının internete erişimi olup olmadığını belirlemek için kullanılabilir. true değeri, internete erişim olduğunu gösterirken, false değeri internete erişimin olmadığını gösterir.
     private fun hasInternetConnection(): Boolean {
         val connectivityManager = getApplication<Application>().getSystemService(
             Context.CONNECTIVITY_SERVICE
@@ -120,24 +100,7 @@ class MainViewModel @Inject constructor(
             else -> false
         }
     }
-
-
-    /**
-     * APPLICATION NE İŞE YARAR?
-     * application parametresi, MainViewModel sınıfının bir bağımlılığını temsil eden Application sınıfının örneğidir. Bu parametre, ViewModel'in Android bileşenlerine (örneğin, aktivite veya uygulama) bağımlı olmasını ve bu bileşenlerle etkileşime girebilmesini sağlar.
-
-    AndroidViewModel sınıfı, Android bileşenlerine özgü işlevselliği sağlamak üzere tasarlanmış bir ViewModel sınıfıdır. Bu sınıf, uygulama Context'ine erişmek için Application sınıfının bir örneğini gerektirir. ViewModel'in Application sınıfını almasının nedeni, Android bileşenleriyle etkileşim kurarken genellikle uygulama Context'ine ihtiyaç duyulmasıdır.
-
-    Örneğin, ViewModel içinde uygulama ayarlarını almak, kaynaklara (dize, çizim, renk vb.) erişmek veya diğer Android bileşenleriyle (örneğin, başka bir aktivite başlatmak) etkileşime girmek gerekebilir. Bu nedenle, ViewModel'in Application örneğine ihtiyaç duyduğu ve application parametresi aracılığıyla bu bağımlılığı enjekte ettiği görülüyor.
-
-    Bununla birlikte, application parametresinin kullanımı uygulama bağlamına özgüdür ve genel olarak ViewModel'in Android bileşenleriyle uyumlu bir şekilde çalışabilmesini sağlar.*/
-
-    /**
-     * Bu yapı, yemek tarifi verilerini almak ve bu verilere erişimi yönetmek için kullanılır.
-     * recipesResponse değişkeni, API isteklerinin sonucunu saklar ve diğer bileşenlerin bu sonuçları gözlemlemesini sağlar. getRecipes fonksiyonu ise yemek tarifi verilerini almak için bir API isteği yapar ve sonucu recipesResponse üzerinden günceller.*/
 }
-
-
 
 /*
 * @HiltViewModel
